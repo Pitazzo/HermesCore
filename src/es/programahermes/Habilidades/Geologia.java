de440ea572af712eee48ee7a8583c8139fb26663
Pitazzo <pedromalo@live.com>
2013-12-27 17:27:44 +0100

    Añadido sistema de extracción de celdas de energía

--- src/es/programahermes/Habilidades/Geologia.java
+++ src/es/programahermes/Habilidades/Geologia.java
@@ -1,24 +1,29 @@
 package es.programahermes.Habilidades;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import es.programahermes.MySQL;
 
 public class Geologia implements Listener {
-	
 
-	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
 		Player player = event.getPlayer();
 		Material material = event.getBlock().getType();
 		double level = MySQL.getLevel(player);
 		if (MySQL.getHability(player).equals("Geologia")) {
-			
+
 			if (material.equals(Material.STONE)) {
 				double stone = 2;
 				MySQL.addPoints(player, stone / level);
@@ -38,9 +43,52 @@ public class Geologia implements Listener {
 
 			if (material.equals(Material.REDSTONE_ORE)) {
 				double redstone = 15;
-				MySQL.addPoints(player, redstone/level);
+				MySQL.addPoints(player, redstone / level);
+			}
+
+			if (material.equals(Material.COAL_ORE)) {
+				ItemStack drop = new ItemStack(Material.STONE, 1);
+				event.getBlock().getDrops(drop);
+				player.sendMessage(ChatColor.RED
+						+ "¡Has malgastado un mineral del que extraer energía!");
 			}
 
 		}
 	}
+
+	// celdas de energia
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		Action action = event.getAction();
+		Player player = event.getPlayer();
+		Block block = event.getClickedBlock();
+		ItemStack tool = new ItemStack(Material.GOLD_SPADE, 1);
+		ItemStack cell = new ItemStack(Material.COAL, 1);
+		Location location = block.getLocation();
+		double random = Math.random() * 10;
+		if (MySQL.getHability(player).equals("Geologia")) {
+			if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
+				if (block.getType().equals(Material.COAL_ORE)) {
+					if (player.getItemInHand().equals(tool)) {
+						if (random >= 2.0) {
+							block.setType(Material.AIR);
+							player.getWorld().dropItemNaturally(location, cell);
+							player.getWorld().playSound(location, Sound.FIZZ,
+									0.5F, 0.0F);
+							player.sendMessage(ChatColor.GREEN
+									+ "¡Has obtenido una celda de energía");
+							tool.setDurability((short) -1);
+						} else {
+							player.getWorld().createExplosion(location, 1.0F);
+							player.sendMessage(ChatColor.RED
+									+ "Parece que algo ha fallado");
+						}
+
+					}
+				}
+			}
+		}
+
+	}
+
 }
