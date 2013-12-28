2914c9962196495012a93aa6278744a05ffa67f4
Pitazzo <pedromalo@live.com>
2013-12-28 17:32:17 +0100

    Bugs corregidos

--- src/es/programahermes/Utilidades/Prospeccion.java
+++ src/es/programahermes/Utilidades/Prospeccion.java
@@ -17,7 +17,6 @@ public class Prospeccion implements Listener {
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		Block block = event.getClickedBlock();
 		ItemStack wood = new ItemStack(Material.WOOD_PICKAXE);
 		ItemStack stone = new ItemStack(Material.STONE_PICKAXE);
 		ItemStack iron = new ItemStack(Material.IRON_PICKAXE);
@@ -25,47 +24,49 @@ public class Prospeccion implements Listener {
 
 		if (MySQL.getHability(player).equals("Geologia")) {
 			if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+				Block block = event.getClickedBlock();
 				if (block.getType().equals(Material.STONE)) {
 					if (player.getItemInHand().equals(wood)
 							|| player.getItemInHand().equals(stone)
 							|| player.getItemInHand().equals(iron)
 							|| player.getItemInHand().equals(diamond)) {
-						
-						double random = Math.random()*10;
-						if(random >= 6.0){
-							
-							block.setType(Material.COBBLESTONE);
-							
-						}else{
-							double random2 = Math.random()*10;
-							Location loc = player.getLocation();
-							int y = loc.getBlockY();
-							
-							//diamond & redstone & esmerald
-							if(y<=16 && y>= 10){
-								
-								if(random2>9.5){
-									block.setType(Material.DIAMOND);
+						if (block != null) {
+
+							double random = Math.random() * 10;
+							if (random >= 6.0) {
+
+								block.setType(Material.COBBLESTONE);
+
+							} else {
+								double random2 = Math.random() * 10;
+								Location loc = player.getLocation();
+								int y = loc.getBlockY();
+
+								// diamond & redstone & esmerald
+								if (y <= 16 && y >= 10) {
+
+									if (random2 > 9.5) {
+										block.setType(Material.DIAMOND_ORE);
+									}
+									if (random2 > 8.5 && random2 < 9.5) {
+										block.setType(Material.REDSTONE_ORE);
+									}
+
 								}
-								if(random2>8.5 &&random2<9.5){
-									block.setType(Material.REDSTONE_ORE);
+								// gold
+								if (y <= 32 && y >= 17) {
+
 								}
-								
-							}
-							//gold
-							if(y<=32 && y>= 17){
-								
-							}
-							//iron
-							if(y<=64 && y>= 33){
-								
-							}
-							//coal
-							if(y<=200 && y>= 65){
-								
+								// iron
+								if (y <= 64 && y >= 33) {
+
+								}
+								// coal
+								if (y <= 200 && y >= 65) {
+
+								}
 							}
 						}
-
 					}
 				}
 			}
