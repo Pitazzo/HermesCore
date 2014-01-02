b534658797945fe2686db7c8b2e51b8d632e12d4
Pitazzo <pedromalo@live.com>
2014-01-02 02:30:47 +0100

    Añadidos nuevos materiales

--- src/es/programahermes/Habilidades/Geologia.java
+++ src/es/programahermes/Habilidades/Geologia.java
@@ -1,19 +1,15 @@
 package es.programahermes.Habilidades;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
-import es.programahermes.MySQL;
 
+import es.programahermes.MySQL;
 
 public class Geologia implements Listener {
 
@@ -24,8 +20,20 @@ public class Geologia implements Listener {
 		int level = MySQL.getLevel(player);
 		if (MySQL.getHability(player).equals("Geologia")) {
 
+			if (material.equals(Material.SANDSTONE)) {
+				double sandstone = 2.5;
+				MySQL.addPoints(player, sandstone / level);
+			}
+			if (material.equals(Material.SAND)) {
+				double sand = 0.5;
+				MySQL.addPoints(player, sand / level);
+			}
+			if (material.equals(Material.GRAVEL)) {
+				double gravel = 0.5;
+				MySQL.addPoints(player, gravel / level);
+			}
 			if (material.equals(Material.STONE)) {
-				
+
 				double stone = 2;
 				MySQL.addPoints(player, stone / level);
 			}
@@ -57,46 +65,16 @@ public class Geologia implements Listener {
 		}
 	}
 
-	
-	// celdas de energia 
 	@EventHandler
-	public void onInteract(PlayerInteractEvent event) {
-		Action action = event.getAction();
+	public void onBuild(BlockPlaceEvent event) {
 		Player player = event.getPlayer();
-		Block block = event.getClickedBlock();
-		ItemStack tool = new ItemStack(Material.GOLD_SPADE, 1);
-		ItemStack cell = new ItemStack(Material.COAL, 1);
-		ItemStack escoria = new ItemStack(Material.STONE, 1);
-		
-		double random = Math.random() * 10;
-		int level = MySQL.getLevel(player);
-		if (MySQL.getHability(player).equals("Geologia")) {
-			if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
-				if (block.getType().equals(Material.COAL_ORE)) {
-					if (player.getItemInHand().equals(tool)) {
-						if (random >= 1.5) {
-							Location location = block.getLocation();
-							block.setType(Material.AIR);
-							player.getWorld().dropItemNaturally(location, cell);
-							player.getWorld().dropItemNaturally(location, escoria);
-							player.getWorld().playSound(location, Sound.FIZZ,
-									0.5F, 0.0F);
-							player.sendMessage(ChatColor.GREEN
-									+ "¡Has obtenido una celda de energía");
-							int coal = 7;
-							MySQL.addPoints(player, coal/level);
-							tool.setDurability((short) -1);
-						} else {
-							
-							player.getWorld().createExplosion(block.getLocation(), 2.0F);
-							player.sendMessage(ChatColor.RED
-									+ "Parece que algo ha fallado");
-						}
-
-					}
-				}
+		if (MySQL.getHability(player).equals("Geologoa")) {
+			if (event.getBlock().getType().equals(Material.FENCE)) {
+				double fence = 4;
+				int level = MySQL.getLevel(player);
+				MySQL.addPoints(player, fence / level);
 			}
 		}
-
 	}
+
 }
