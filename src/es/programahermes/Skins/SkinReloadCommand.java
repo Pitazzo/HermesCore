f53cdc65b6f624a1b77cf1848ac7f23a0a8cb39e
pitazzo <pedromalo@live.com>
2014-10-02 00:06:53 +0200

    Cambios menores

--- src/es/programahermes/Skins/SkinReloadCommand.java
+++ src/es/programahermes/Skins/SkinReloadCommand.java
@@ -1,5 +1,6 @@
 package es.programahermes.Skins;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,11 +11,36 @@ public class SkinReloadCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 		if (cmd.getName().equalsIgnoreCase("recargarskin")) {
-			if(sender instanceof Player){
-				Player player = (Player) sender;
-				SkinManager.setSkin(player);
-				return true;
+			if (args.length == 0) {
+				if (sender instanceof Player) {
+					Player player = (Player) sender;
+					SkinManager.setSkin(player);
+					return true;
+				}
+			} else if (args.length == 1) {
+				if (sender.isOp()) {
+					Player player = null;
+
+					try {
+						player = Bukkit.getPlayer(args[0]);
+					} catch (Exception e){}
+
+					if (player != null) {
+						SkinManager.setSkin(player);
+						System.out.println("[ADM DE SKINS] La skin de "
+								+ args[0] + " ha sido actualizada");
+						player = null;
+						return true;
+					} else {
+						System.out.println("[ADM DE SKINS] El jugador "
+								+ args[0] + " no se encuetra online");
+						player = null;
+						return true;
+					}
+					
+				}
 			}
+
 		}
 		return false;
 	}
